b5aec30520d57711e6b7302c5404e1a058dc258c
Pierre-Luc Carmel Biron <plbc@hotmail.com>
2020-10-07 10:56:15 -0400

    Add comments and apply small modifications.

--- speed-controller.ino
+++ speed-controller.ino
@@ -1,26 +1,24 @@
-/* pulse width */
-#define PERIOD 50000.0  /*  µs  */
-#define OFF_WIDTH 800.0  /*  µs  */
-#define MIN_WIDTH 1000.0  /*  µs  */
-#define MAX_WIDTH 1600.0  /*  µs  */
+// pulse width
+#define PERIOD 50000.0  /* µs */
+#define OFF_WIDTH 800.0  /* µs */
+#define MIN_WIDTH 1000.0  /* µs */
+#define MAX_WIDTH 1600.0  /* µs */
+#define WIDTH_RANGE (MAX_WIDTH - MIN_WIDTH)
 
-/* rate of pulse width change */
-#define MIN_ACCELERATION 10.0  /*  µ/s  */
-#define MAX_ACCELERATION 1000.0  /*  µ/s  */
+// rate of pulse width change
+#define MIN_ACCELERATION 10.0  /* µ/s */
+#define MAX_ACCELERATION 1000.0  /* µ/s */
 #define ACCELERATION_LINEAR_MAX 1000 /* 0 - 1023 */
+#define ACCELERATION_RANGE (MAX_ACCELERATION - MIN_ACCELERATION)
 
-#define BUTTON_IN 2
-#define SPEED_IN 1
-#define ACCELERATION_IN 5
-#define STATE_LED_OUT 12
-#define PWM_OUT 13
+#define BUTTON_IN 2 /* connected to a button that sets the pin to the ground when pressed */
+#define SPEED_IN 1 /* pin connected to a 4.7K potentiometer through a small 10 ohms series resistance */
+#define ACCELERATION_IN 5 /* pin connected to a 4.7K potentiometer through a small 10 ohms series resistance */
+#define STATE_LED_OUT 12 /* connected to a led through a 10K series resistance */
+#define PWM_OUT 13 /* output signal */
 
-#define BUTTON_DEBOUNCING_TIME 500L /*  ms  */
-
-#define WIDTH_RANGE ((MAX_WIDTH - MIN_WIDTH))
-#define ACCELERATION_RANGE ((MAX_ACCELERATION - MIN_ACCELERATION))
-
-#define SECOND_TO_MICROS 1000000.0
+// to avoid multiple triggers when the button is pressed
+#define BUTTON_DEBOUNCING_TIME 500L /* ms */
 
 volatile unsigned long lastButtonInterrupt = 0;
 volatile bool runState = false;
@@ -31,7 +29,6 @@ float readRequestedWidth() {
 
 float readRequestedAcceleration() {
   int val = analogRead(ACCELERATION_IN);
-  Serial.println(val); 
   return val > ACCELERATION_LINEAR_MAX
     ? INFINITY
     : MIN_ACCELERATION + ACCELERATION_RANGE * (float)val / (float)ACCELERATION_LINEAR_MAX;
@@ -39,7 +36,7 @@ float readRequestedAcceleration() {
 
 void onButtonDebounced() {
   unsigned long currentTime = micros();
-  if (((long)currentTime - lastButtonInterrupt) >= BUTTON_DEBOUNCING_TIME * 1000) {
+  if ((long)currentTime - lastButtonInterrupt >= BUTTON_DEBOUNCING_TIME * 1000) {
     onButton();
     lastButtonInterrupt = currentTime;
   }
@@ -64,19 +61,9 @@ void updateWidth() {
   float requestedAcceleration = readRequestedAcceleration();
   float diff = requestedWidth - lastWidth;
   float dir = diff ? (diff / abs(diff)) : 0.0;
-  float delta = requestedAcceleration == INFINITY ? diff : (dir * requestedAcceleration * PERIOD) / SECOND_TO_MICROS;
+  float delta = requestedAcceleration == INFINITY ? diff : (dir * requestedAcceleration * PERIOD) / 1000000.0;
   float a = dir * diff;
   delta = constrain(delta, -a, a);
-  // Serial.print("last: ");
-  // Serial.print(lastWidth);
-  // Serial.print(" | requested: ");
-  // Serial.print(requestedWidth);
-  // Serial.print("| requestedAcceleration: ");
-  // Serial.print(requestedAcceleration);
-  // Serial.print("| dir: ");
-  // Serial.print(dir);
-  // Serial.print("| delta: ");
-  // Serial.println(delta);
   lastWidth = lastWidth + delta;
 }
 
@@ -92,7 +79,6 @@ void waitNextPeriod() {
 }
 
 void setup() {
-  // Serial.begin(9600);
   pinMode(BUTTON_IN, INPUT_PULLUP);
   pinMode(STATE_LED_OUT, OUTPUT);
   pinMode(PWM_OUT, OUTPUT);
